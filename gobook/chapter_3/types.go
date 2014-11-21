45973d1c2d34f1dc725478f4d8a82468a1504fde
Christoph Voigt <voigt.christoph@gmail.com>
2014-11-21 13:45:41 +0100

    added chapters 2 and 3

---
+++ gobook/chapter_3/types.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+    fmt.Println("1 + 1 =", 1 + 1)
+    fmt.Println(len("Hello World"))
+    fmt.Println("Hello World"[1])
+    fmt.Println("Hello " + "World")
+    fmt.Println(true && false);
+}
